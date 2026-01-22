a51510d1bb5aa0a87ff972a8d040331750d81cc8
Andre Ferreira <andregmf25@gmail.com>
2026-01-22 14:03:26 +0000

    Add segurança sem encriptação

---
+++ src/main/java/com/bibliotecaandre/biblioteca/securityconfig/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.bibliotecaandre.biblioteca.securityconfig;
+
+import com.bibliotecaandre.biblioteca.model.User;
+import com.bibliotecaandre.biblioteca.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
